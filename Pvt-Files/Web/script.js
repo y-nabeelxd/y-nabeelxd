7a2eb4c811d76d163a817cf9c9719d707eeee8a0
Nabeel XD <nabeelxd99@gmail.com>
2024-10-23 09:46:28 +0530

    .

---
+++ Pvt-Files/Web/script.js
@@ -0,0 +1,11 @@
+// Theme Toggle
+document.getElementById('theme-toggle').addEventListener('click', () => {
+  const body = document.body;
+  if (body.classList.contains('dark-theme')) {
+    body.classList.replace('dark-theme', 'light-theme');
+    document.getElementById('theme-toggle').textContent = '🌙';
+  } else {
+    body.classList.replace('light-theme', 'dark-theme');
+    document.getElementById('theme-toggle').textContent = '☀️';
+  }
+});
